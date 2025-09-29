0736765e7db96e0c8e0a673706cfebbf51eab77c
YuSa0-6 <fuku.sugar@icloud.com>
2025-09-29 23:49:42 +0900

    INSERTモードから出た時にIMEをOFFにする。
    ref: https://namileriblog.com/mac/neovim-imselect/

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,18 +1,8 @@
 return {
   "keaising/im-select.nvim",
   config = function()
-    require("im_select").setup({
-      -- デフォルトの入力方法（英語）
-      default_im_select = "com.apple.keylayout.ABC",
-      
-      -- 前の入力方法を復元するかどうか
-      default_command = "im-select",
-      
-      -- インサートモードを抜ける時だけ英語に切り替え
-      set_default_events = { "InsertLeave" },
-      
-      -- 前の入力方法を復元しない
-      set_previous_events = {},
-    })
+    require('im_select').setup {
+      default_im_select = "com.apple.keylayout.ABC"
+    }
   end,
 }
